c474fdbe131e0e69f368d3409e74e18dae5c234a
Mahesh S Warrier <maheshs60@gmail.com>
2016-06-06 17:29:28 +0530

    First commit of FormatterTrait.php

---
+++ FormatterTrait.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package   yii2-formatter
+ * @author    Mahesh S Warrier <maheshs60@gmail.com>
+ * @copyright Copyright &copy; Mahesh S Warrier, 2016
+ * @version   1.0.0
+ */
+namespace common\traits\base;
+
+trait FormatterTrait{
+    public $format;
+    private $methods;
+    public function __get($name){
+        $this->methods = get_class_methods($this);
+        foreach($this->methods as $method){
+            $key = lcfirst(str_replace("get", "", $method));
+            if(strpos($name, $key) !== FALSE){ 
+                $actualValue = $this->{lcfirst(str_replace($key, "", $name))};
+                return $this->{"get".ucfirst($key)}($actualValue);
+            }
+        }
+        return parent::__get($name);
+    }
+
+    public function getDateFormatted($value){
+        return date('Y', strtotime($value));
+    }
+
+    public function getMoneyFormatted($value){
+        return Helper::money($value);
+    }
+}
